2f6ce02c5b0f2599a220ccd6a5cda7f0bb9c58e3
Andrii Kalinichenko <andreykalina@mail.ru>
2015-11-22 20:22:48 +0200

    Initial commit

---
+++ blog/migrations/0002_auto_20151122_1830.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.utils.timezone import utc
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='create_date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 11, 22, 16, 30, 5, 494637, tzinfo=utc)),
+        ),
+    ]
